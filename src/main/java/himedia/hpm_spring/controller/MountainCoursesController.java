e4b1597d2d29613733ca37ef27a97ef456f680e3
kim5303123 <36145674+kim5303123@users.noreply.github.com>
2025-04-09 10:38:45 +0900

    [승룡] 어노테이션 추가

--- src/main/java/himedia/hpm_spring/controller/MountainCoursesController.java
+++ src/main/java/himedia/hpm_spring/controller/MountainCoursesController.java
@@ -2,6 +2,7 @@ package himedia.hpm_spring.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,13 +17,14 @@ import himedia.hpm_spring.service.MountainCoursesService;
 @RequestMapping("/api/mountains")
 public class MountainCoursesController {
 
+	@Autowired
 	private MountainCoursesService mountainCoursesService;
 
 	 @GetMapping("/{id}/courses")
 	    public List<MountainCoursesVo> getCoursesByMountainId(@PathVariable Long id) {
 	        try {            
 	            return mountainCoursesService.retrieveMountainById(id);
-	        } catch (Exception e) {
+	        } catch (Exception e) {	        	
 	            throw new ResponseStatusException(
 	                HttpStatus.INTERNAL_SERVER_ERROR,
 	                "코스 정보 조회 실패: " + e.getMessage()
